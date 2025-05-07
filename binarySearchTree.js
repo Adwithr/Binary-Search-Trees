f220d0cbb953aad68db08f9b822f60aaa9540355
Adwithr <154674132+Adwithr@users.noreply.github.com>
2025-05-07 20:09:48 +0530

    buildTree method

---
+++ binarySearchTree.js
@@ -0,0 +1,36 @@
+import { Node } from "./nodes.js";
+
+class Tree {
+  constructor(arr) {
+    const sortedUniqueArray = [...new Set(arr)].sort((a, b) => a - b);
+    this.root = this.buildTree(sortedUniqueArray, 0, sortedUniqueArray.length - 1);
+  }
+
+  buildTree(arr, startIndex, endIndex) {
+    if (startIndex > endIndex) return null;
+
+    const mid = Math.floor((startIndex + endIndex) / 2);
+    const node = new Node(arr[mid]);
+
+    node.left = this.buildTree(arr, startIndex, mid - 1);
+    node.right = this.buildTree(arr, mid + 1, endIndex);
+
+    return node;
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const idk = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(idk.root);
